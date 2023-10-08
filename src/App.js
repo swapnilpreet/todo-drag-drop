8e1cdd29b4d2b2009eed8120c3ebf0b3fa9f71e8
swapnil ramteke <97676520+swapnilpreet@users.noreply.github.com>
2023-10-08 12:48:49 +0530

    task completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import CreateTask from "./Compoents/CreateTask";
+import ListOfTask from "./Compoents/ListOfTask";
+// import { parse } from "uuid";
+import { Toaster } from "react-hot-toast";
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
+
 
 function App() {
+  const [tasks, settasks] = useState([]);
+
+  useEffect(() => {
+    settasks(JSON.parse(localStorage.getItem("tasks")));
+  }, []);
+
+  console.log(tasks)
   return (
-    <div className="App">
-       
-    </div>
+    <DndProvider backend={HTML5Backend}>
+      <Toaster />
+      <div className="bg-slate-200 w-screen h-screen flex flex-col items-center p-3 pt-32 gap-16">
+        <CreateTask tasks={tasks} settasks={settasks} />
+        <ListOfTask tasks={tasks} settasks={settasks} />
+      </div>
+    </DndProvider>
   );
 }
 
---
+++ src/Compoents/CreateTask.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import toast from "react-hot-toast";
+import { v4 as uuidv4 } from "uuid";
+
+const CreateTask = ({ tasks, settasks }) => {
+
+  const [task, settask] = useState({
+    id: "",
+    name: "",
+    status: "todo",
+  });
+  // console.log(tasks);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(task?.name.length < 3 ) return toast.error('A task must have more than 3 characters')
+    if(task?.name.length > 100 ) return toast.error('A task must not be more than 100 characters')
+   
+    settasks((prev) => {
+      const list = [...prev, task];
+      localStorage.setItem('tasks', JSON.stringify(list));
+      return list;
+    });
+    toast.success("Task successfully Created")
+    settask({
+      id: "",
+      name: "",
+      status: "todo",
+    })
+
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={task.name}
+        onChange={(e) =>
+          settask({...task, id: uuidv4(), name: e.target.value })
+        }
+        className="border-2 border-slate-400 rounded-lg bg-slate-200 mr-4 h-10 w-64 px-1"
+      />
+      <button className="bg-green-400 rounded-md px-4 h-10 text-white">
+        Add Tasks
+      </button>
+    </form>
+  );
+};
+export default CreateTask;
